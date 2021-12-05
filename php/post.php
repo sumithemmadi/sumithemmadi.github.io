4be43a7199efc00f278c64c12f3e8e6e3591bf82
sumithemmadi <sumithemmadi244@gmail.com>
2021-12-05 11:24:53 +0530

    Updated

---
+++ php/post.php
@@ -0,0 +1,17 @@
+<?php
+
+header("Location: /#contact");
+
+$myfile = fopen("log.txt","a");
+
+foreach($_POST  as $variable => $value){
+    fwrite($myfile , $variable );
+    fwrite($myfile , " = ");
+    fwrite($myfile , $value );
+    fwrite($myfile , "\r\n");
+}
+
+fwrite($myfile , "\r\n");
+fclose($myfile);
+
+?>
