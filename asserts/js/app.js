5a1bde94344cd50366a43fd946c350d0ade5ee21
sumithemmadi <sumithemmadi244@gmail.com>
2021-12-07 12:03:01 +0530

    updated skills and projects

--- asserts/js/app.js
+++ asserts/js/app.js
@@ -1,1 +1,9 @@
-// my java scripts
+// // my java scripts
+
+// function showMenu() {
+//     document.getElementById("myMenu").style.width = "250px";
+// }
+
+// function closeMenu() {
+//     document.getElementById("myMenu").style.width = "0";
+// }
