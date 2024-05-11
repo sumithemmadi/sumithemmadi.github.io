ba8509bf36297ffbe9e5b0f27dd1c1c39b457026
Emmadi Sumith Kumar <sumithemmadi244@gmail.com>
2024-05-11 10:10:08 +0530

    Create inject.js

---
+++ public/inject.js
@@ -0,0 +1,9 @@
+// function removeRootElements() {
+//   const rootElements = document.querySelectorAll('.root');
+
+//   for (const element of rootElements) {
+//     element.parentNode.removeChild(element);
+//   }
+// }
+
+// setTimeout(removeRootElements, 10000);
